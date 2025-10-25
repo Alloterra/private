2c4f8ab40f96e55ce6d84c5c7e0f1f7131074b1c
Somark Lenka <lenkasomark@gmail.com>
2025-10-26 04:13:45 +0530

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -euo pipefail
+: "${URL:?URL not set}"
+
+mkdir -p /workspace
 cd /workspace
-wget -O entry.sh $URL
-chmod +x /workspace/entry.sh
-/workspace/entry.sh
+wget -O entry.sh "$URL"
+chmod u+x entry.sh
+./entry.sh
